220fccc80d4988127052a1cc65fc5d4c76859642
Hariprasath <86220744+prasath0405@users.noreply.github.com>
2024-04-09 00:22:59 +0530

    Update render.js

--- server/service/render.js
+++ server/service/render.js
@@ -6,7 +6,7 @@ exports.prasath=(req,res)=>{
 }
 let axios = require('axios')
 exports.datasfile = (req,res)=>{
-    axios.get("http://localhost:2199/api/post").then(function (response) {
+    axios.get("http://localhost:10000/api/post").then(function (response) {
         console.log(response);
         res.render('datas',{user:response.data})
     })
